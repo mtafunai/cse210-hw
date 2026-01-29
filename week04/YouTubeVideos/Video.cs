910d35c3f553ae02fd11e9a3f4f1b0b1d67ec296
mtafunai <roy.tafunai10@gmail.com>
2026-01-29 15:37:01 +1200

    semi colon missing

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 public class Video
 {
-    public string Title {get; set;}
-    public string Author {get; set}
-    public int LengthInSeconds {get; set;}
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
 
     private List<Comment> _comments = new List<Comment>();
 
