d8b892ef771e5d1461fd21914797f722d6a0cf66
mtafunai <roy.tafunai10@gmail.com>
2026-02-14 22:34:13 +1200

    updated

--- week06/EternalQuest/EternalGoals.cs
+++ week06/EternalQuest/EternalGoals.cs
@@ -1,7 +1,7 @@
 public class EternalGoal : Goal
 {
-    {
     public EternalGoal(string name, string description, int points) : base (name, description, points)
+    {
     }
 
 public override int RecordEvent()
